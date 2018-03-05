93dcda4aa20cd127453947d4818d95a9e9f5284e
David Whitney <david.whitney@thetrainline.com>
2018-03-05 17:51:25 +0000

    Init.

---
+++ CodingGameBottersOfTheUniverse/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodingGameBottersOfTheUniverse
+{
+    public class Player
+    {
+        public static void Main(string[] args)
+        {
+            var gameState = GetGameState();
+            
+            while (true)
+            {
+                var turnState = GetTurnState();
+
+                // Write an action using Console.WriteLine()
+                // To debug: Console.Error.WriteLine("Debug messages...");
+
+
+                // If roundType has a negative value then you need to output a Hero name, such as "DEADPOOL" or "VALKYRIE".
+                // Else you need to output roundType number of any valid action, such as "WAIT" or "ATTACK unitId"
+                Console.WriteLine("WAIT");
+            }
+        }
+
+        #region Parsing
+        private static TurnState GetTurnState()
+        {
+            var turnState = new TurnState
+            {
+                Gold = int.Parse(Console.ReadLine()),
+                EnemyGold = int.Parse(Console.ReadLine()),
+                RoundType =
+                    int.Parse(Console.ReadLine()), // a positive value will show the number of heroes that await a command
+                EntityCount = int.Parse(Console.ReadLine())
+            };
+            for (var i = 0; i < turnState.EntityCount; i++)
+            {
+                var turnInput = Console.ReadLine().Split(' ').ToList();
+                turnState.Units.Add(new Unit
+                {
+                    UnitId = int.Parse(turnInput[0]),
+                    Team = int.Parse(turnInput[1]),
+                    UnitType = turnInput[2], // UNIT, HERO, TOWER, can also be GROOT from wood1
+                    X = int.Parse(turnInput[3]),
+                    Y = int.Parse(turnInput[4]),
+                    AttackRange = int.Parse(turnInput[5]),
+                    Health = int.Parse(turnInput[6]),
+                    MaxHealth = int.Parse(turnInput[7]),
+                    Shield = int.Parse(turnInput[8]), // useful in bronze
+                    AttackDamage = int.Parse(turnInput[9]),
+                    MovementSpeed = int.Parse(turnInput[10]),
+                    StunDuration = int.Parse(turnInput[11]), // useful in bronze
+                    GoldValue = int.Parse(turnInput[12]),
+                    CountDown1 = int.Parse(turnInput[13]), // all countDown and mana variables are useful starting in bronze
+                    CountDown2 = int.Parse(turnInput[14]),
+                    CountDown3 = int.Parse(turnInput[15]),
+                    Mana = int.Parse(turnInput[16]),
+                    MaxMana = int.Parse(turnInput[17]),
+                    ManaRegeneration = int.Parse(turnInput[18]),
+                    HeroType = turnInput[19], // DEADPOOL, VALKYRIE, DOCTOR_STRANGE, HULK, IRONMAN
+                    IsVisible = int.Parse(turnInput[20]), // 0 if it isn't
+                    ItemsOwned = int.Parse(turnInput[21]), // useful from wood1
+                });
+            }
+            return turnState;
+        }
+
+        private static GameState GetGameState()
+        {
+            List<string> inputs;
+            var state = new GameState
+            {
+                MyTeam = int.Parse(Console.ReadLine())
+            };
+
+            var bushAndSpawnPointCount = int.Parse(Console.ReadLine()); // usefrul from wood1, represents the number of bushes and the number of places where neutral units can spawn
+            for (var i = 0; i < bushAndSpawnPointCount; i++)
+            {
+                inputs = Console.ReadLine().Split(' ').ToList();
+                state.Entities.Add(new Entity
+                {
+                    EntityType = inputs[0],// BUSH, from wood1 it can also be SPAWN
+                    X = int.Parse(inputs[1]),
+                    Y = int.Parse(inputs[2]),
+                    Radius = int.Parse(inputs[3])
+                });
+            }
+
+            var itemCount = int.Parse(Console.ReadLine()); // useful from wood2
+            for (var i = 0; i < itemCount; i++)
+            {
+                inputs = Console.ReadLine().Split(' ').ToList();
+                state.Items.Add(new Item
+                {
+                    ItemName = inputs[0], // contains keywords such as BRONZE, SILVER and BLADE, BOOTS connected by "_" to help you sort easier
+                    ItemCost = int.Parse(inputs[1]), // BRONZE items have lowest cost, the most expensive items are LEGENDARY
+                    Damage = int.Parse(inputs[2]), // keyword BLADE is present if the most important item stat is damage
+                    Health = int.Parse(inputs[3]),
+                    MaxHealth = int.Parse(inputs[4]),
+                    Mana = int.Parse(inputs[5]),
+                    MaxMana = int.Parse(inputs[6]),
+                    MoveSpeed = int.Parse(inputs[7]), // keyword BOOTS is present if the most important item stat is moveSpeed
+                    ManaRegeneration = int.Parse(inputs[8]),
+                    IsPotion = int.Parse(inputs[9]), // 0 if it's not instantly consumed
+                });
+            }
+            return state;
+        }
+        #endregion
+    }
+
+    public class GameState
+    {
+        public int MyTeam { get; set; }
+        public List<Entity> Entities { get; set; } = new List<Entity>();
+        public List<Item> Items { get; set; } = new List<Item>();
+    }
+
+    public class TurnState
+    {
+        public int EntityCount { get; set; }
+        public int Gold { get; set; }
+        public int EnemyGold { get; set; }
+        public int RoundType { get; set; }
+        public List<Unit> Units { get; set; } = new List<Unit>();
+    }
+
+    public class Unit
+    {
+        public int UnitId { get; set; }
+        public int Team { get; set; }
+        public string UnitType { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int AttackRange { get; set; }
+        public int Health { get; set; }
+        public int MaxHealth { get; set; }
+        public int Shield { get; set; }
+        public int AttackDamage { get; set; }
+        public int MovementSpeed { get; set; }
+        public int StunDuration { get; set; }
+        public int GoldValue { get; set; }
+        public int CountDown1 { get; set; }
+        public int CountDown2 { get; set; }
+        public int CountDown3 { get; set; }
+        public int Mana { get; set; }
+        public int MaxMana { get; set; }
+        public int ManaRegeneration { get; set; }
+        public string HeroType { get; set; }
+        public int IsVisible { get; set; }
+        public int ItemsOwned { get; set; }
+    }
+
+    public class Item
+    {
+        public string ItemName { get; set; }
+        public int ItemCost { get; set; }
+        public int Damage { get; set; }
+        public int Health { get; set; }
+        public int MaxHealth { get; set; }
+        public int Mana { get; set; }
+        public int MaxMana { get; set; }
+        public int MoveSpeed { get; set; }
+        public int ManaRegeneration { get; set; }
+        public int IsPotion { get; set; }
+    }
+
+    public class Entity
+    {
+        public string EntityType { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Radius { get; set; }
+    }
+}
